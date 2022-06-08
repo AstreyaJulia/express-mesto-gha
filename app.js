4f6f2315210e981be74bd35c0301122fed86324c
Юлия Латышева <58434576+AstreyaJulia@users.noreply.github.com>
2022-06-08 21:15:21 +0300

    FIX: не видит .eslintrc

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /** Коннект к MongoDB */
-mongoose.connect('mongodb://localhost:27017/mestodb'); // Mongoose 6 не нужны параметры
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req, res, next) => {
   req.user = {
